4de64752cefa42c919445ff64c6ebb452c5f4e0a
Roman Gertsvolf <roman.gertsvolf@gmail.com>
2020-01-29 10:02:11 +0200

    fixes02

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var WIZARDS_NUMBER = 4;
+var WIZARDS_AMOUNT = 4;
 var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARDS_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARDS_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -14,7 +14,7 @@ var getRandomElement = function (thisArray) {
 var getWizardsArray = function (names, surnames, coatColors, eyeColors) {
   var wizards = [];
 
-  for (var i = 0; i < WIZARDS_NUMBER; i++) {
+  for (var i = 0; i < WIZARDS_AMOUNT; i++) {
     wizards[i] = {};
     wizards[i].name = getRandomElement(names);
     wizards[i].surname = getRandomElement(surnames);
@@ -40,7 +40,7 @@ var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
 setup.querySelector('.setup-similar').classList.remove('hidden');
 
-var similarListElement = document.querySelector('.setup-similar-list');
+var similarListElement = setup.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
@@ -48,7 +48,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
 var wizardsArray = getWizardsArray(WIZARDS_NAMES, WIZARDS_SURNAMES, WIZARDS_COAT_COLORS, WIZARDS_EYES_COLORS);
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i < WIZARDS_NUMBER; i++) {
+for (var i = 0; i < WIZARDS_AMOUNT; i++) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement = addWizard(wizardsArray[i], wizardElement);
   fragment.appendChild(wizardElement);
